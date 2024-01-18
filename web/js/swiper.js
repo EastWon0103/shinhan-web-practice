f88d46ec8bb294cdb4e6eaf9881343727bef4d02
EastWon0103 <dongwon000103@gmail.com>
2024-01-18 11:17:08 +0900

    feat: my portfolio

---
+++ web/js/swiper.js
@@ -0,0 +1,11 @@
+if (window.innerWidth >= 520) {
+    const swiper = new Swiper('.swiper', {
+        speed: 1000,
+        slidesPerView: 'auto',
+        spaceBetween: 100,
+        centerSlides: true,
+        centeredSlidesBounds: true,
+        slidesOffsetBefore: 400,
+        slidesOffsetAfter: 400,
+    });
+}
